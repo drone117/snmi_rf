c86b246ad48fc8b12bc2e7571442d5e5fba30b66
V_The_Titan_Of_Programming <wizardy.oni@gmail.com>
2022-05-23 21:41:08 +0300

    Работа с тест

--- NewsBots/testing.py
+++ NewsBots/testing.py
@@ -1,11 +1,12 @@
 import logging
 import pytz
-import fetch
+#import fetch
 import telegram.ext
 import datetime
 
 # from datetime import datetime
 # from datetime import time
+from fetch import fetcher
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Updater
 from telegram.ext import CallbackContext
@@ -15,12 +16,13 @@ from telegram.ext import MessageHandler, Filters, InlineQueryHandler
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-latest_news = fetch.latest_news_text
+#latest_news = fetch.latest_news_text
 
 user_hour = 13
 user_minute = 0
 user_time = f'{user_hour}:{user_minute}'
-
+kektext = 'kektext'
+#kektext = fetcher.fetch_data()
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Добро пожаловать! Данный бот присылает новостной "
@@ -76,27 +78,31 @@ def settime(update, context):
 
 
 def latest(update, context):
-    if len(latest_news) > 4096:
-        for x in range(0, len(latest_news), 4096):
-            context.bot.send_message(chat_id=update.effective_chat.id, text=latest_news[x:x + 4096],
-                                     parse_mode=telegram.ParseMode.MARKDOWN)
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text=latest_news,
-                                 parse_mode=telegram.ParseMode.MARKDOWN)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=kektext, parse_mode=telegram.ParseMode.MARKDOWN)
+    # if len(latest_news) > 4096:
+    #     # for x in range(0, len(latest_news), 4096):
+    #         # if latest_news[x + 4095] == "*":
+    #         #     latest_news[x + 4096] = ''
+    #     context.bot.send_message(chat_id=update.effective_chat.id, text=latest_news[0:4083],
+    #                                  parse_mode=telegram.ParseMode.MARKDOWN)
+    # else:
+    #     context.bot.send_message(chat_id=update.effective_chat.id, text=latest_news,
+    #                              parse_mode=telegram.ParseMode.MARKDOWN)
 
 def text(context):
     context.bot.send_message(context.job.context, text='test', disable_notification=False)
 
 
 def auto_news(context):
-
-    if len(latest_news) > 4096:
-        for x in range(0, len(latest_news), 4096):
-            context.bot.send_message(context.job.context, text=latest_news[x:x + 4096],
-                                     parse_mode=telegram.ParseMode.MARKDOWN)
-    else:
-        context.bot.send_message(context.job.context, text=latest_news,
-                                 parse_mode=telegram.ParseMode.MARKDOWN)
+    context.bot.send_message(context.job.context, text='kekw',
+                             parse_mode=telegram.ParseMode.MARKDOWN)
+    # if len(latest_news) > 4096:
+    #     for x in range(0, len(latest_news), 4096):
+    #         context.bot.send_message(context.job.context, text=latest_news[x:x + 4096],
+    #                                  parse_mode=telegram.ParseMode.MARKDOWN)
+    # else:
+    #     context.bot.send_message(context.job.context, text=latest_news,
+    #                              parse_mode=telegram.ParseMode.MARKDOWN)
 
 
 def error(update, context):
@@ -104,23 +110,23 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-def main():
-    updater = Updater(token='5381998359:AAH-fTcEBFzbuvGc4hHDmqbaKpS3PZFcOmQ', use_context=True)
-    dispatcher = updater.dispatcher
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
-    settime_handler = CommandHandler('settime', settime)
-    dispatcher.add_handler(settime_handler)
-    latest_handler = CommandHandler('latest', latest)
-    dispatcher.add_handler(latest_handler)
-    stop_handler = CommandHandler('stop', stop)
-    dispatcher.add_handler(stop_handler)
-    time_handler = MessageHandler(Filters.text & (~Filters.command), time)
-    dispatcher.add_handler(time_handler)
-    dispatcher.add_error_handler(error)
-    updater.start_polling()
-    updater.idle()
+#def main():
+updater = Updater(token='5293833835:AAEjnJjdkacWHMkWta6FsA7ivtEmMtuEZy0', use_context=True)
+dispatcher = updater.dispatcher
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+settime_handler = CommandHandler('settime', settime)
+dispatcher.add_handler(settime_handler)
+latest_handler = CommandHandler('latest', latest)
+dispatcher.add_handler(latest_handler)
+stop_handler = CommandHandler('stop', stop)
+dispatcher.add_handler(stop_handler)
+time_handler = MessageHandler(Filters.text & (~Filters.command), time)
+dispatcher.add_handler(time_handler)
+dispatcher.add_error_handler(error)
+updater.start_polling()
+updater.idle()
 
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+   # main()
